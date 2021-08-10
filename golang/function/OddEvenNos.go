fcb7724adaac4ebca7408364b7e535ce22b8940f
shehbab <kakkar.shehbab@gmail.com>
2021-08-10 11:59:17 +0530

    Create OddEvenNos.go

---
+++ golang/function/OddEvenNos.go
@@ -0,0 +1,25 @@
+package main
+import ("fmt";"os";"strconv")
+func checkNo(n string){
+   // fmt.Printf("No entered is %q\n",n)
+    p, _ := strconv.ParseFloat(n, 64)         //Convert String to Float
+    fmt.Println("The number entered:",p)
+    p1 := int(p)                             //Convert Float to Int
+    fmt.Printf("%.1f convert to int value = %d\n",p,p1)
+    if p1 % 2 == 0 {
+      fmt.Printf("%d is even\n",p1)
+    } else {
+      fmt.Printf("%d is odd\n",p1)
+    }
+}
+func start(){
+   if len(os.Args) != 2 {
+      fmt.Println("Enter the Single Number.")
+      return
+   }
+   n := os.Args[1]
+   checkNo(n)
+}
+func main(){
+    start()
+}
